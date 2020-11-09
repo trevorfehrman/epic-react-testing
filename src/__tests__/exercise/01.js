d779a555717b57fba8eaf23a69b55eef5ecb8678
Trevor Fehrman <trevorfehrman@gmail.com>
2020-11-09 00:03:23 -0800

    some stuff?

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -7,22 +7,32 @@ import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
   // 🐨 create a div to render your component to (💰 document.createElement)
+  const node = document.createElement('div')
   //
   // 🐨 append the div to document.body (💰 document.body.append)
+  document.body.append(node)
   //
   // 🐨 use ReactDOM.render to render the <Counter /> to the div
+  ReactDOM.render(<Counter />, node)
   // 🐨 get a reference to the increment and decrement buttons:
-  //   💰 div.querySelectorAll('button')
+  const buttons = node.querySelectorAll('button')
   // 🐨 get a reference to the message div:
-  //   💰 div.firstChild.querySelector('div')
+  const message = node.firstChild.querySelector('div')
   //
   // 🐨 expect the message.textContent toBe 'Current count: 0'
+  expect(message.textContent).toBe('Current count: 0')
   // 🐨 click the increment button (💰 increment.click())
+  buttons[1].click()
+  expect(message.textContent).toBe('Current count: 1')
+
+  buttons[0].click()
+  expect(message.textContent).toBe('Current count: 0')
   // 🐨 assert the message.textContent
   // 🐨 click the decrement button (💰 decrement.click())
   // 🐨 assert the message.textContent
   //
   // 🐨 cleanup by removing the div from the page (💰 div.remove())
+  node.remove()
   // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
 })
 
--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -3,9 +3,16 @@
 
 import * as React from 'react'
 // 🐨 you'll need to grab waitForElementToBeRemoved from '@testing-library/react'
-import {render, screen} from '@testing-library/react'
+import {
+  render,
+  screen,
+  wait,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
+import {rest} from 'msw'
+import {setupServer} from 'msw/node'
 // 🐨 you'll need to import rest from 'msw' and setupServer from msw/node
 import Login from '../../components/login-submission'
 
@@ -17,16 +24,21 @@ const buildLoginForm = build({
 })
 
 // 🐨 get the server setup with an async function to handle the login POST request:
-// 💰 here's something to get you started
-// rest.post(
-//   'https://auth-provider.example.com/api/login',
-//   async (req, res, ctx) => {},
-// )
+const server = setupServer(
+  rest.post(
+    'https://auth-provider.example.com/api/login',
+    async (req, res, ctx) => {
+      return res(ctx.json({username: req.body.username}))
+    },
+  ),
+)
 // you'll want to respond with an JSON object that has the username.
 // 📜 https://mswjs.io/
 
 // 🐨 before all the tests, start the server with `server.listen()`
+beforeAll(() => server.listen())
 // 🐨 after all the tests, stop the server with `server.close()`
+afterAll(() => server.close())
 
 test(`logging in displays the user's username`, async () => {
   render(<Login />)
@@ -35,14 +47,16 @@ test(`logging in displays the user's username`, async () => {
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.type(screen.getByLabelText(/password/i), password)
   // 🐨 uncomment this and you'll start making the request!
-  // userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
   // as soon as the user hits submit, we render a spinner to the screen. That
   // spinner has an aria-label of "loading" for accessibility purposes, so
   // 🐨 wait for the loading spinner to be removed using waitForElementToBeRemoved
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
   // 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitforelementtoberemoved
 
   // once the login is successful, then the loading spinner disappears and
   // we render the username.
   // 🐨 assert that the username is on the screen
+  expect(screen.getByText(username)).toBeInTheDocument()
 })
